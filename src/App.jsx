93caa4a9106eb5220cfd0151cb8899dd2de4ec4c
sanskarsharma100 <Thebestone@123>
2024-02-03 17:42:21 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-700">Hello World</div>
+    </>
+  );
+}
+
+export default App;
